0e0af084ae9dafd5bf16012aa44430b4fadfa267
Raares2005 <rares.croitoru8@gmail.com>
2023-05-15 01:19:33 +0300

    FIXED TOATE PAGINILE INAFARA DE POZE DE PE MAIN

--- NAV.js
+++ NAV.js
@@ -1,24 +1,40 @@
-let menuIcon = document.querySelector('.menuIcon');
-let nav = document.querySelector('.overlay-menu');
+const btn = document.getElementById("containerLogoBurger");
+const backdrop = document.getElementById("backdrop");
+const btnServicii = document.getElementById("btnServicii");
+const btnServiciiBurger = document.getElementById("btnBurgerDropdownContainer");
+const body = document.getElementById("body")
 
-menuIcon.addEventListener('click', () => {
-    if (nav.style.transform != 'translateX(0%)') {
-        nav.style.transform = 'translateX(0%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    } else { 
-        nav.style.transform = 'translateX(-100%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    }
-});
-
-
-// Toggle Menu Icon ========================================
-let toggleIcon = document.querySelector('.menuIcon');
-
-toggleIcon.addEventListener('click', () => {
-    if (toggleIcon.className != 'menuIcon toggle') {
-        toggleIcon.className += ' toggle';
+if (btn && backdrop) {
+    const bLogo = document.getElementById("containerLogoBurger");
+    const bContainer = document.getElementById("burgerListContainer");
+    let widthB = body.offsetWidth;
+  const bContainerUl = document.getElementById("containerBurgerStuff");
+  btn.addEventListener("click", function () {
+    if (!(bLogo.classList.contains("open")) && widthB > 500) {
+      bLogo.classList.add("open");
+      backdrop.style.display = "block";
+      bContainer.style.width = "300px";
+      bContainerUl.style.display = "flex";
+      body.style.overflow = "hidden";
+    }else if (!(bLogo.classList.contains("open")) && widthB <= 500) {
+      bLogo.classList.add("open");
+      backdrop.style.display = "block";
+      bContainer.style.width = "200px";
+      bContainerUl.style.display = "flex";
+      body.style.overflow = "hidden";
     } else {
-        toggleIcon.className = 'menuIcon';
+      bLogo.classList.remove("open");
+      backdrop.style.display = "none";
+      bContainer.style.width = "0px";
+      bContainerUl.style.display = "none";
+      body.style.overflow = "visible";
     }
-});
+  });
+  backdrop.addEventListener("click", function () {
+    bLogo.classList.remove("open");
+    backdrop.style.display = "none";
+    bContainer.style.width = "0px";
+    bContainerUl.style.display = "none";
+    body.style.overflow = "visible";
+  });
+}
