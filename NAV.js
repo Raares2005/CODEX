484e8f5e38dd8fa94bc97e02863c9c080df16ee8
Raares2005 <rares.croitoru8@gmail.com>
2023-05-15 01:08:12 +0300

    REVENIRE LA NORMAL

--- NAV.js
+++ NAV.js
@@ -1,3 +1,40 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eb42f8e67613bf462400204f987329aef945e9845437786bc8de74ce3cdc099a
-size 1606
+const btn = document.getElementById("containerLogoBurger");
+const backdrop = document.getElementById("backdrop");
+const btnServicii = document.getElementById("btnServicii");
+const btnServiciiBurger = document.getElementById("btnBurgerDropdownContainer");
+const body = document.getElementById("body")
+
+if (btn && backdrop) {
+    const bLogo = document.getElementById("containerLogoBurger");
+    const bContainer = document.getElementById("burgerListContainer");
+    let widthB = body.offsetWidth;
+  const bContainerUl = document.getElementById("containerBurgerStuff");
+  btn.addEventListener("click", function () {
+    if (!(bLogo.classList.contains("open")) && widthB > 500) {
+      bLogo.classList.add("open");
+      backdrop.style.display = "block";
+      bContainer.style.width = "300px";
+      bContainerUl.style.display = "flex";
+      body.style.overflow = "hidden";
+    }else if (!(bLogo.classList.contains("open")) && widthB <= 500) {
+      bLogo.classList.add("open");
+      backdrop.style.display = "block";
+      bContainer.style.width = "200px";
+      bContainerUl.style.display = "flex";
+      body.style.overflow = "hidden";
+    } else {
+      bLogo.classList.remove("open");
+      backdrop.style.display = "none";
+      bContainer.style.width = "0px";
+      bContainerUl.style.display = "none";
+      body.style.overflow = "visible";
+    }
+  });
+  backdrop.addEventListener("click", function () {
+    bLogo.classList.remove("open");
+    backdrop.style.display = "none";
+    bContainer.style.width = "0px";
+    bContainerUl.style.display = "none";
+    body.style.overflow = "visible";
+  });
+}
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:24d25fad74f6998fd39ab72d2b5e5c462b20d86face13c59730e3b86abe06ee4
-size 7
+
